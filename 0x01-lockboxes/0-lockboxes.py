0f48736a9239a75f7f41b60da933fe2ebb5e93b2
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-05-03 03:28:39 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+"""check if all doors can be opened"""
+
+
+def canUnlockAll(boxes):
+    if len(boxes) == 0:
+        return False
+
+    keys = [x for x in boxes[0]]
+    openedBoxes = set()
+
+    while keys:
+        key = keys.pop(0)
+        if key not in openedBoxes and key < len(boxes):
+            if len(boxes[key]) == 0:
+                openedBoxes.add(key)
+            keys.extend(boxes[key])
+            openedBoxes.add(key)
+            if check_if_opened(len(boxes), openedBoxes):
+                return True
+
+    return (False)
+
+
+def check_if_opened(count, listB):
+    if count == len(listB):
+        return True
+    return False
