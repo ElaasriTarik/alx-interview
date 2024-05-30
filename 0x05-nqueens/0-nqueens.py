eb5e2870e3b39ecb869152591e45f0d7d903b6ee
ElaasriTarik <elaasriya2003@gmail.com>
2024-05-30 23:33:26 +0100

    init

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -11,7 +11,7 @@ def printSolution(board, n):
             if board[i][j] == 1:
                 
                 solutions.append([i, j])
-    print(solutions)
+    return(solutions)
 
 def createTemplate(n):
     board = []
@@ -28,27 +28,18 @@ def createTemplate(n):
 def nqueens(board, col):
     if col >= n:
         return True
- 
-    # Consider this column and try placing
-    # this queen in all rows one by one
+
     for i in range(n):
  
         if isSafe(board, i, col):
- 
-            # Place this queen in board[i][col]
             board[i][col] = 1
  
             # Recur to place rest of the queens
             if nqueens(board, col + 1) == True:
                 return True
  
-            # If placing queen in board[i][col
-            # doesn't lead to a solution, then
-            # queen from board[i][col]
             board[i][col] = 0
  
-    # If the queen can not be placed in any row in
-    # this column col then return false
     return False
 
 
@@ -57,19 +48,17 @@ def isSafe(board, row, col):
     for i in range(col):
         if board[row][i] == 1:
             return False
- 
-    # Check upper diagonal on left side
+
     for i, j in zip(range(row, -1, -1),
                     range(col, -1, -1)):
         if board[i][j] == 1:
             return False
- 
-    # Check lower diagonal on left side
+
     for i, j in zip(range(row, n, 1),
                     range(col, -1, -1)):
         if board[i][j] == 1:
             return False
- 
+
     return True
 
 
