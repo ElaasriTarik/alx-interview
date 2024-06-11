3809a0c85684679ce18e03fe1af064348d11b432
ElaasriTarik <elaasriya2003@gmail.com>
2024-06-11 12:29:00 +0100

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,19 +3,28 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, async function (error, response, body) {
-    if (error) return (error);
+  if (error) return (error);
 
-    const res = JSON.parse(body);
-    // console.log(res.characters);
-    printCharacters(res.characters);
+  const characters = JSON.parse(body).characters;
+  for (const key in characters) {
+    const result = await new Promise((resolve, reject) => {
+      request(characters[key], function (error, response, body) {
+        if (error) return (error);
+        resolve(JSON.parse(body).name);
+      });
+    }
+    );
+    console.log(result);
+  }
+  // printCharacters(res.characters);
 });
 
-async function printCharacters(characters) {
-    for (const character of characters) {
-        if (character === undefined) { return; }
-        const url = character;
-        await fetch(url)
-            .then(response => response.json())
-            .then(data => console.log(data.name));
-    }
-}
+// async function printCharacters (characters) {
+//   for (const character of characters) {
+//     if (character === undefined) { return; }
+//     const url = character;
+//     await fetch(url)
+//       .then(response => response.json())
+//       .then(data => console.log(data.name));
+//   }
+// }
