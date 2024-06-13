5e93a7235e4fbe2ea844d45a45aceeac7c07621a
ElaasriTarik <elaasriya2003@gmail.com>
2024-06-13 16:05:54 +0100

    first shot

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topLeft = matrix[top][left + i]
+
+            matrix[top][left + i] = matrix[bottom - i][left]
+
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+
+            matrix[bottom][right - i] = matrix[top + i][right]
+
+            matrix[top + i][right] = topLeft
+
+        left += 1
+        right -= 1
+#     print_matrix(matrix)
+
+# def print_matrix(matrix):
+#     for row in matrix:
+#         print(' '.join(map(str, row)))
