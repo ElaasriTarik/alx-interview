ec21d3df923392eb88a357a9de152f629afeaa09
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-06-27 23:26:15 +0100

    requirement fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,29 +7,26 @@
 def island_perimeter(grid):
     """island perimeter function"""
 
-    max_len = len(grid[0]) - 1
+    max_len = len(grid[0])
     perimeter = 0
 
-    for arr in range(max_len):
-        for i in range(max_len + 1):
+    for arr in range(max_len - 1):
+        for i in range(0, max_len):
             # check first and last lands
-            if i == 1 and i == max_len + 1 or i == max_len + 1 and i == 1:
-                perimeter += 1
-            # check next land
-            if grid[arr][i] == 1 and grid[arr][i + 1] == 0:
-                perimeter += 1
-            # check prev land
-            if grid[arr][i] == 1 and grid[arr][i - 1] == 0:
-                perimeter += 1
-            # check the top land
-            if grid[arr][i] == 1 and grid[arr - 1][i] == 0:
-                perimeter += 1
-            # check the bottom land
-            if grid[arr][i] == 1 and grid[arr + 1][i] == 0:
-                perimeter += 1
+            if grid[arr][i] == 1:
+                if i == 0 or i == max_len:
+                    perimeter += 1
+                # check next land
+                if grid[arr][i + 1] == 0:
+                    perimeter += 1
+                # check prev land
+                if grid[arr][i - 1] == 0:
+                    perimeter += 1
+                # check the top land
+                if grid[arr - 1][i] == 0:
+                    perimeter += 1
+                # check the bottom land
+                if grid[arr + 1][i] == 0:
+                    perimeter += 1
 
     return perimeter
-
-
-if __name__ == "__main__":
-    island_perimeter()
