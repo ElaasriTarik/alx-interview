af0a8c4ea9104350508f57243a6f60b5ee45abde
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-06-27 23:33:00 +0100

    requirement fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,22 +9,23 @@ def island_perimeter(grid):
 
     max_len = len(grid[0])
     perimeter = 0
-
-    for arr in range(len(grid)):
+    grid_len = len(grid)
+    for arr in range(grid_len):
         for i in range(0, max_len):
             # check first and last lands
             if grid[arr][i] == 1:
-                # Check left edge or left cell is water
+                
+                # check first cell or the prev
                 if i == 0 or grid[arr][i - 1] == 0:
                     perimeter += 1
-                # Check right edge or right cell is water
+                # check last row or next row land
+                if arr == grid_len - 1 or grid[arr + 1][i] == 0:
+                    perimeter += 1
+                # chcek last cell or next land
                 if i == max_len - 1 or grid[arr][i + 1] == 0:
                     perimeter += 1
-                # Check top edge or top cell is water
+                # check first row or prev row land
                 if arr == 0 or grid[arr - 1][i] == 0:
-                    perimeter += 1
-                # Check bottom edge or bottom cell is water
-                if arr == len(grid) - 1 or grid[arr + 1][i] == 0:
                     perimeter += 1
 
     return perimeter
